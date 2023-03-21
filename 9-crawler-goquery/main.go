fab09a509359b856cdb3795274ef3ecd787de0d6
Kdsingh333 <90763155+Kdsingh333@users.noreply.github.com>
2023-03-22 00:27:04 +0530

    Crawler-Goquery

---
+++ 9-crawler-goquery/main.go
@@ -0,0 +1,44 @@
+package main
+
+import (
+	"fmt"
+	"log"
+
+	"net/http"
+
+	"github.com/PuerkitoBio/goquery"
+)
+
+func Check(err error) {
+	if err != nil {
+		log.Println(err)
+	}
+
+}
+func main() {
+	blogTitles, err := GetLatestBlogTitles("https://golangcode.com")
+	Check(err)
+
+	fmt.Println("Blog titles")
+	fmt.Printf(blogTitles)
+
+}
+
+func GetLatestBlogTitles(url string) (string, error) {
+	resp, err := http.Get(url)
+	if err != nil {
+		return " ", err
+	}
+
+	doc, err := goquery.NewDocumentFromReader(resp.Body)
+
+	if err != nil {
+		return " ", err
+	}
+	titles := ""
+	doc.Find(".post-title").Each(func(i int, s *goquery.Selection) {
+		titles += "-" + s.Text() + "\n"
+	})
+
+	return titles, nil
+}
